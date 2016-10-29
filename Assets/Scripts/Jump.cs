0e470363355f8558741a72bdcd4453dc1a257c39
Sergej Tihonov <sergej.tihonov@brutalhack.com>
2016-10-29 16:05:07 +0200

    created a bug with a jump script.

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+	private Vector3 fromPosition;
+	private Vector3 toPosition;
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			fromPosition = Input.mousePosition;
+		}
+		else if (Input.GetMouseButtonUp(0))
+		{
+			toPosition = Input.mousePosition;
+			jump(fromPosition-toPosition);
+		}
+	}
+
+	private void jump(Vector3 jumpVector)
+	{
+		GetComponent<Rigidbody>().AddForce(jumpVector);
+	}
+}
