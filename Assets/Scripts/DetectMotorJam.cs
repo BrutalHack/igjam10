7a0e0527b46a8ff1a12b9fef4a147b2519e66997
Benjamin Justice <benjamin.justice@brutalhack.com>
2016-10-29 05:14:57 +0200

    IGJ10-12: Detection of player-crushing via script on motorized cogwheel. The cogwheel can only detect a jam in general, not if the bug is stuck in between or something else. Thus this solution does not work if we do want to jam cogwheels manually.

---
+++ Assets/Scripts/DetectMotorJam.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class DetectMotorJam : MonoBehaviour
+{
+    public int MaxTorque = 50;
+    private HingeJoint2D _hingeJoint2D;
+
+    // Use this for initialization
+    void Start()
+    {
+        _hingeJoint2D = GetComponent<HingeJoint2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var motorTorque = _hingeJoint2D.GetMotorTorque(0.1f);
+        if (motorTorque > MaxTorque)
+        {
+            Debug.Log("RESET!");
+            Debug.Break();
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+//        Debug.Log(collision.contacts[0].collider.gameObject.name + ": " +
+//                  collision.contacts[0].collider.gameObject.GetComponent<HingeJoint2D>().GetMotorTorque(0.1f));
+//        Debug.Log("velocity: " + collision.relativeVelocity + ";     inertia: " + this.GetComponent<Rigidbody2D>().inertia);
+    }
+}
