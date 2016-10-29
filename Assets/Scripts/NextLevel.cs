36eb8c37198a103aaacf89d1bdcee52f2237fefb
Sergej Tihonov <sergej.tihonov@brutalhack.com>
2016-10-30 00:53:16 +0200

    create a simple start scene

---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    public int nextSceneId = 1;
+
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(nextSceneId);
+    }
+}
