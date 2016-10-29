eedb83b7794f4d4156bbb45d6c606e445e26cb01
Sergej Tihonov <sergej.tihonov@brutalhack.com>
2016-10-29 22:38:46 +0200

    Create Bug animation

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -3,14 +3,20 @@ using System.Collections;
 
 public class Jump : MonoBehaviour
 {
+    private Animator animator;
     private Rigidbody playerRigidbody;
     [SerializeField] private bool isGrounded;
     public LayerMask GroundLayers;
     public float GroundCheckLength = 0.0f;
+    private Vector3 jumpVector;
+    private bool flyUp = false;
+    private bool flyTop = false;
+    private bool flyDown = true;
 
     // Use this for initialization
     void Start()
     {
+        animator = GetComponent<Animator>();
         playerRigidbody = GetComponent<Rigidbody>();
         GroundCheckLength += GetComponent<CapsuleCollider>().height / 2;
         Debug.Log("Length: " + GroundCheckLength);
@@ -20,6 +26,38 @@ public class Jump : MonoBehaviour
     void Update()
     {
         UpdateIsGrounded();
+        CheckVelocityForAnimation();
+    }
+
+    public void PrepareJump()
+    {
+        if (isGrounded)
+        {
+            animator.SetTrigger("Start");
+        }
+    }
+
+    private void CheckVelocityForAnimation()
+    {
+        if (flyUp && !flyTop && playerRigidbody.velocity.y == 0)
+        {
+            flyTop = true;
+            Debug.Log("Top");
+            animator.SetTrigger("Top");
+        }else if (!flyDown && playerRigidbody.velocity.y < -0.5)
+        {
+            flyDown = true;
+            Debug.Log("Down");
+            animator.SetBool("FlyDown", true);
+        }
+        else if (flyDown && isGrounded)
+        {
+            animator.SetTrigger("Land");
+            animator.SetBool("FlyDown", false);
+            flyDown = false;
+            flyTop = false;
+            flyUp = false;
+        }
     }
 
     private void UpdateIsGrounded()
@@ -39,11 +77,16 @@ public class Jump : MonoBehaviour
         Debug.DrawLine(transform.position, transform.position + relativeDownFront * GroundCheckLength, Color.yellow);
     }
 
+
+
     public void StartJump(Vector3 jumpVector)
     {
         if (isGrounded)
         {
             playerRigidbody.AddForce(jumpVector, ForceMode.Impulse);
+            animator.SetTrigger("Up");
+            Debug.Log("Up");
+            flyUp = true;
         }
     }
 }
--- Assets/Scripts/PlayerMouseInput.cs
+++ Assets/Scripts/PlayerMouseInput.cs
@@ -14,6 +14,7 @@ public class PlayerMouseInput : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             fromPosition = Input.mousePosition;
+            GetComponent<Jump>().PrepareJump();
         }
         else if (Input.GetMouseButtonUp(0))
         {
