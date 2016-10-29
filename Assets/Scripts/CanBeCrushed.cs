5d8ab7f50e21ccd556e2d57c0bec741d23245b4b
Benjamin Justice <benjamin.justice@brutalhack.com>
2016-10-29 12:00:56 +0200

    3D Physics instead of 2D; detection of when the bedbug is crushed

---
+++ Assets/Scripts/CanBeCrushed.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanBeCrushed : MonoBehaviour
+{
+    public float MaximumPressure = 2.0f;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        MaximumPressure = 2f;
+        if (collision.impulse.magnitude > MaximumPressure)
+        {
+            Debug.Log("Wanze wurde zerquestscht");
+        }
+    }
+}
