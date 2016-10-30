62ad85fb3f4857b35823f725d06520c29de9bf72
Sergej Tihonov <sergej.tihonov@brutalhack.com>
2016-10-30 12:53:20 +0100

    Updated Bug Grafics

--- Assets/Scripts/Drop.cs
+++ Assets/Scripts/Drop.cs
@@ -1,10 +1,18 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Drop : MonoBehaviour
 {
     public Texture[] partTexture = new Texture[6];
     public GameObject part;
     private int position = 0;
+    public Material[] Materials = new Material[6];
+    public Row[] Textures = new Row[7];
+
+    void Start()
+    {
+        UpdateMaterials(0);
+    }
 
     public void drop()
     {
@@ -12,13 +20,28 @@ public class Drop : MonoBehaviour
         {
             dropPart(position);
             position++;
+            UpdateMaterials(position);
         }
     }
 
     private void dropPart(int partId)
     {
-        Vector3 position = transform.position + new Vector3(0, -0.5f, 0);
+        Vector3 position = transform.position + new Vector3(0, -(transform.lossyScale.y / 2), 0);
         GameObject bodyPart = (GameObject) Instantiate(part, position, Quaternion.identity);
         bodyPart.GetComponent<MeshRenderer>().material.mainTexture = partTexture[partId];
     }
+
+    private void UpdateMaterials(int position)
+    {
+        for (int i = 0; i < Materials.Length; i++)
+        {
+            Materials[i].mainTexture = Textures[position].Textures[i];
+        }
+    }
+}
+
+[Serializable]
+public class Row
+{
+    public Texture[] Textures = new Texture[6];
 }
